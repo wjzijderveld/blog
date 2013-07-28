08bfc9e9da2848a1958f194155d2ff0ac747ddcd
Willem-Jan Zijderveld <willemjan@beeldspraak.com>
2013-07-28 12:39:29 +0200

    Fixed deployment some more :)

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -22,13 +22,3 @@ set :deploy_via, :remote_cache
 set :deploy_to, "/var/customers/webs/wjzijderveld/blog.willem-jan.net"
 set :user, "wjzijderveld"
 set :use_sudo, false
-
-
-# If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
