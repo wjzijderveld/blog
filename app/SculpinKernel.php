3428aabc9a7596868c84938f3de1eb40c63d04b2
Willem-Jan Zijderveld <willemjan@beeldspraak.com>
2014-02-05 20:32:13 +0100

    Added RelatedContentBundle

--- app/SculpinKernel.php
+++ app/SculpinKernel.php
@@ -12,6 +12,7 @@ class SculpinKernel extends AbstractKernel
     protected function getAdditionalSculpinBundles()
     {
         return array(
+            'Wjzijderveld\Sculpin\RelatedContentBundle\WjzijderveldSculpinRelatedContentBundle',
         );
     }
 
